058760b00b6c200c143af7e1b7924e74287ac0f4
mikehadlow <mikehadlow@42d4aba2-f048-0410-9719-5941d86a54b7>
2008-05-07 15:42:01 +0000

    Added QueriableExtensions

---
+++ Suteki.Shop/Suteki.Common/Repositories/QueriableExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Suteki.Common.Repositories
+{
+    public static class QueriableExtensions
+    {
+        const string expression_not_property = "propertyExpression must be a property accessor. e.g: 'x => x.MyProperty'";
+
+        public static IQueryable<T> WithSelectItem<T>(this IQueryable<T> items, Expression<Func<T, string>> propertyExpression) 
+            where T : new()
+        {
+            if (propertyExpression.Body.NodeType != ExpressionType.MemberAccess)
+            {
+                throw new ArgumentException(expression_not_property);
+            }
+            MemberExpression memberExpression = propertyExpression.Body as MemberExpression;
+            PropertyInfo property = memberExpression.Member as PropertyInfo;
+            if (property == null)
+            {
+                throw new ArgumentException(expression_not_property);
+            }
+
+            T selectItem = new T();
+            property.SetValue(selectItem, "<select>", null);
+
+            var selectItems = new List<T> { selectItem }.AsQueryable();
+            return selectItems.Union(items);
+        }
+    }
+}
