cb45beb16701ce259855cdb6e6b2d2a66d9cfee3
mikehadlow <mikehadlow@42d4aba2-f048-0410-9719-5941d86a54b7>
2008-05-12 09:04:57 +0000

    Moved Paging to Suteki.Common. Not removed from Suteki Shop yet.

---
+++ Suteki.Shop/Suteki.Common/HtmlHelpers/HtmlHelperExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Suteki.Common.Extensions;
+
+namespace Suteki.Common.HtmlHelpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string Pager(
+            this HtmlHelper htmlHelper,
+            string controller,
+            string action,
+            IPagedList pagedList)
+        {
+            Pager pageListBuilder = new Pager(htmlHelper, controller, action, pagedList);
+            return pageListBuilder.WriteHtml();
+        }
+    }
+}
