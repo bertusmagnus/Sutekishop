d75170ed294d57f109b48187e54d5ce4d1004d7c
mikehadlow <mikehadlow@42d4aba2-f048-0410-9719-5941d86a54b7>
2008-06-04 12:07:47 +0000

    Rebuilt Castle and MVCContrib from current trunk. Note: MvcContrib refereces an older build of Castle, so I've updated MvcContrib.Castle.dll to use the latest. Replacing this MvcContrib.Castle.dll with one built from trunk will break the application.

--- Suteki.Shop/Suteki.Shop/Models/OrderSearchCriteria.cs
+++ Suteki.Shop/Suteki.Shop/Models/OrderSearchCriteria.cs
@@ -9,11 +9,5 @@ namespace Suteki.Shop
         public string Postcode { get; set; }
         public string Lastname { get; set; }
         public int OrderStatusId { get; set; }
-
-//        public bool HasOrderId { get { return OrderId != 0; } }
-//        public bool HasEmail { get { return !string.IsNullOrEmpty(Email); } }
-//        public bool HasPostcode { get { return !string.IsNullOrEmpty(Postcode); } }
-//        public bool HasLastname { get { return !string.IsNullOrEmpty(Lastname); } }
-//        public bool HasOrderStatusId { get { return OrderStatusId != 0; } }
     }
 }
