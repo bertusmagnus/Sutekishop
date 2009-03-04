f3d69f26060de004c817b0a502510ebf12a07717
Jeremy.Skinner <Jeremy.Skinner@42d4aba2-f048-0410-9719-5941d86a54b7>
2009-03-04 20:11:47 +0000

    LeftMenu now rendered by MenuController

--- Suteki.Shop/Suteki.Shop/Controllers/MenuController.cs
+++ Suteki.Shop/Suteki.Shop/Controllers/MenuController.cs
@@ -6,16 +6,23 @@ namespace Suteki.Shop.Controllers
 {
 	public class MenuController : Controller
 	{
-		private IRepository<Content> contentRepository;
+		private readonly IRepository<Content> contentRepository;
+		private readonly IRepository<Category> categoryRepository;
 
-		public MenuController(IRepository<Content> contentRepository)
+		public MenuController(IRepository<Content> contentRepository, IRepository<Category> categoryRepository)
 		{
 			this.contentRepository = contentRepository;
+			this.categoryRepository = categoryRepository;
 		}
 
 		public ActionResult MainMenu()
 		{
 			return View(contentRepository.GetMainMenu());
+		}
+
+		public ActionResult LeftMenu()
+		{
+			return View(categoryRepository.GetRootCategory());
 		}
 	}
 }
---
+++ Suteki.Shop/Suteki.Shop/Views/Menu/LeftMenu.ascx
@@ -0,0 +1,2 @@
+<%@ Control Language="C#" Inherits="System.Web.Mvc.ViewUserControl<Category>" %>
+<%= Html.WriteCategories(Model, CategoryDisplay.View) %>
