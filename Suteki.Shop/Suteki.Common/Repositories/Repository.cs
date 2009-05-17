bb788f179574d67b924401a43a6e22042936811b
mikehadlow <mikehadlow@42d4aba2-f048-0410-9719-5941d86a54b7>
2009-05-17 09:28:14 +0000

    Return a nice error when a primary key does not exist.

--- Suteki.Shop/Suteki.Common/Repositories/Repository.cs
+++ Suteki.Shop/Suteki.Common/Repositories/Repository.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Linq.Expressions;
 using System.Data.Linq;
+using System.Runtime.Serialization;
 using Suteki.Common.Extensions;
 
 namespace Suteki.Common.Repositories
@@ -31,7 +32,15 @@ namespace Suteki.Common.Repositories
                 new[] { itemParameter }
                 );
 
-            return GetAll().Where(whereExpression).Single();
+            var item = GetAll().Where(whereExpression).SingleOrDefault();
+
+            if (item == null)
+            {
+                throw new PrimaryKeyNotFoundException(string.Format("No {0} with primary key {1} found",
+                    typeof(T).FullName, id));
+            }
+
+            return item;
         }
 
         public virtual IQueryable<T> GetAll()
@@ -77,6 +86,35 @@ namespace Suteki.Common.Repositories
         object IRepository.GetById(int id)
         {
             return GetById(id);
+        }
+    }
+
+    [Serializable]
+    public class PrimaryKeyNotFoundException : Exception
+    {
+        //
+        // For guidelines regarding the creation of new exception types, see
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
+        // and
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
+        //
+
+        public PrimaryKeyNotFoundException()
+        {
+        }
+
+        public PrimaryKeyNotFoundException(string message) : base(message)
+        {
+        }
+
+        public PrimaryKeyNotFoundException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected PrimaryKeyNotFoundException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
         }
     }
 }
