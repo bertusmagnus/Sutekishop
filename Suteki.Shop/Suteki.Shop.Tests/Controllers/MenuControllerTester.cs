e5a0307f78b1d86b8aaf330eb06265384c257843
mikehadlow <mikehadlow@42d4aba2-f048-0410-9719-5941d86a54b7>
2009-11-01 11:04:39 +0000

    Better tests for CategoryViewDataExtensions

--- Suteki.Shop/Suteki.Shop.Tests/Controllers/MenuControllerTester.cs
+++ Suteki.Shop/Suteki.Shop.Tests/Controllers/MenuControllerTester.cs
@@ -107,24 +107,6 @@ namespace Suteki.Shop.Tests.Controllers
 	            .WithModel<CategoryViewData>();
 
 	        Assert.That(rootCategory.Name, Is.EqualTo("root"));
-            Assert.That(rootCategory.ChildCategories.Count, Is.EqualTo(2), "root has no children");
-
-	        var one = rootCategory.ChildCategories[0];
-	        var two = rootCategory.ChildCategories[1];
-            Assert.That(one.Name, Is.EqualTo("one"));
-            Assert.That(two.Name, Is.EqualTo("two"));
-
-            Assert.That(one.ChildCategories.Count, Is.EqualTo(2), "one has no children");
-	        var oneOne = one.ChildCategories[0];
-	        var oneTwo = one.ChildCategories[1];
-            Assert.That(oneOne.Name, Is.EqualTo("oneOne"));
-            Assert.That(oneTwo.Name, Is.EqualTo("oneTwo"));
-
-            Assert.That(oneTwo.ChildCategories.Count, Is.EqualTo(2), "oneTwo has no children");
-	        var oneTwoOne = oneTwo.ChildCategories[0];
-	        var oneTwoTwo = oneTwo.ChildCategories[1];
-            Assert.That(oneTwoOne.Name, Is.EqualTo("oneTwoOne"));
-            Assert.That(oneTwoTwo.Name, Is.EqualTo("oneTwoTwo"));
 	    }
 	}
 }
